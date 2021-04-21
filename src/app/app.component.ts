036ede70d726c7f2bbbe9ec7cab2662e4c6a6643
losu5 <losu507@tlen.pl>
2021-04-21 14:15:25 +0200

    modifing component.html, component.ts, index.html, styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
+import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
@@ -27,6 +28,27 @@ public getEmployees(): void {
     }
   );
 }
+
+public onOpenModal(employee: Employee, mode: string): void {
+  const container = document.getElementById('main-container');
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.style.display = 'none';
+  button.setAttribute('data-toggle', 'modal');
+  if (mode === 'add') {
+    button.setAttribute('data-target', '#addEmployeeModal');
+  }
+  if (mode === 'edit') {
+    button.setAttribute('data-target', '#updateEmployeeModal');
+  }
+  if (mode === 'delete') {
+    button.setAttribute('data-target', '#deleteEmployeeModal');
+  }
+  container.appendChild(button);
+  button.click();
+}
+
+
 
 
 }
